5730261fb21e4334baeb17b225281bafc74922fb
Eddy <125514698+KarlRow@users.noreply.github.com>
2023-12-11 21:04:54 +0100

    Update ProgramFixtures.php

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -9,48 +9,48 @@ use Doctrine\Persistence\ObjectManager;
 class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
     const PROGRAMS = [
-        'Program1' => [
+        'The Walking dead' => [
                         'title'    => 'The Walking dead',
                         'synopsis' => 'Après une apocalypse ayant transformé la quasi-totalité de la population en zombies, un groupe d\'hommes et de femmes mené par l\'officier Rick Grimes tente de survivre. Ensemble, ils vont devoir tant bien que mal faire face à ce nouveau monde.',
                         'category' => 'Action',
                         'poster'   => '/build/images/the-walking-dead.jpg',
                         'country'  => 'Etat-Unis',
                         'year'     => '2010',],
-        'Program2' => [
+        'Dark' => [
                         'title'    => 'Dark',
                         'synopsis' => 'Lorsque deux enfants disparaissent dans une petite ville allemande, son passé tragique refait surface. Quatre familles à la recherche des enfants vont voir leur routine bouleversée et les secrets de chacun vont être mis en lumière.',
                         'category' => 'Fantastique',
                         'poster'   => '/build/images/dark.jpg',
                         'country'  => 'Allemagne',
                         'year'     => '2017',],
-        'Program3' => [
+        'YOU' => [
                         'title'    => 'YOU',
                         'synopsis' => 'Un charmant libraire répondant au nom de Joe utilise les nouvelles technologies pour attirer la jolie romancière Beck et la faire tomber amoureuse. Son obsession envers Beck devient dangereuse mais Joe est prêt à tout.',
                         'category' => 'Horreur',
                         'poster'   => '/build/images/you.jpg',
                         'country'  => 'Etat-Unis',
                         'year'     => '2018',],
-        'Program4' => [
+        'Vikings Valhalla' => [
                         'title'    => 'Vikings Valhalla',
                         'synopsis' => 'Les aventures de certains des Vikings les plus célèbres de l\'histoire qui ouvrent de nouvelles voies dans une Europe en constante évolution : l\'explorateur Leif Eriksson, sa soeur Freydis Eriksdotter et l\'ambitieux Prince du Nord Harald Sigurdsson.',
                         'category' => 'Aventure',
                         'poster'   => '/build/images/vikings.jpg',
                         'country'  => 'Etat-Unis',
                         'year'     => '2022',],
-        'Program5' => [
+        'L\'Attaque des Titans' => [
                         'title'    => 'L\'Attaque des Titans',
                         'synopsis' => 'Il y a un siècle, l\'humanité a presque été exterminée par des titans qui dévorent les hommes. Les survivants sont regroupés dans une ville fortifiée. C\'est alors qu\'un titan colossal, plus haut que les murs de la ville, se présente devant la cité...',
                         'category' => 'Animation',
                         'poster'   => '/build/images/l_attaque_des_titans.jpg',
                         'country'  => 'Japon',
                         'year'     => '2013 ',],
-        'Program6' => [
+        'Arcane' => [
                         'title'    => 'Arcane',
                         'synopsis' => 'Championnes de leurs villes jumelles et rivales (la huppée Piltover et la sous-terraine Zaun), deux sœurs Vi et Powder se battent dans une guerre où font rage des technologies magiques et des perspectives diamétralement opposées.',
                         'category' => 'Animation',
                         'poster'   => '/build/images/arcane.webp',
                         'country'  => 'Etat-Unis',
-                        'year'     => '2021',],
+                        'year'     => '2021',],          
     ];
     public function load(ObjectManager $manager)
     {
